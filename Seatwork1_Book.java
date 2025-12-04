22feb6f25d2174a07baa66d4a3fbd9d5f61bc1e9
Gabriel A. Bolanos <gabolanos@headoffice.corp.bpi.com.ph>
2025-12-04 16:19:18 +0800

    initial commit

---
+++ Seatwork1_Book.java
@@ -0,0 +1,11 @@
+package OutputName;
+
+public class Book {
+	String title;
+	
+	void read() {
+		System.out.print("Book title is: " + title );
+		
+	} // end of void
+	
+}// end of class
---
+++ Seatwork1_Main.java
@@ -0,0 +1,17 @@
+package OutputName;
+
+import java.util.Scanner;
+
+public class OutputMain {
+	
+		public static void main(String[] args) {
+
+			Book myBook = new Book();
+			myBook.title = "The Lord of the Rings";
+			
+			myBook.read();
+
+			
+		}
+	}
+	
