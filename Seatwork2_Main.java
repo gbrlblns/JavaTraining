7c2ced2a1c642ebbb437dab310bfe0cdb8291b69
Gabriel A. Bolanos <gabolanos@headoffice.corp.bpi.com.ph>
2025-12-04 16:20:42 +0800

    initial commit

---
+++ Seatwork2_Main.java
@@ -0,0 +1,18 @@
+package OutputName;
+
+import java.util.Scanner;
+
+public class OutputMain {
+	
+		public static void main(String[] args) {
+
+			
+			WhiteBoard wb = new WhiteBoard();
+			int sum = wb.addNumbers(5,3);
+			System.out.println("The total is : " + String.valueOf(sum));
+			
+			
+			
+		}
+	}
+	
