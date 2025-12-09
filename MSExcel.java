470ce958ab5e6e40ac358b6fce5a5ec467a8149f
Gabriel A. Bolanos <gabolanos@headoffice.corp.bpi.com.ph>
2025-12-09 17:01:29 +0800

    s4 commit

---
+++ MSExcel.java
@@ -0,0 +1,25 @@
+package OutputName;
+
+public class MSExcel extends Program{
+	
+	public MSExcel(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		String message = "Opening MS Excel...";
+		System.out.println(message);
+	}
+	
+	@Override
+	public void stop() {
+		String message = "Stopping MS Excel...";
+		System.out.println(message);
+	}
+}
+ 
+ 
+
+ 
+ 
